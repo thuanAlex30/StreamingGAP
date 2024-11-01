3ac1e161a942d591be4542df53fb154ed7796a7a
levanhuy12345 <lehuy1234987@gmail.com>
2024-11-01 10:19:25 +0700

    ok

--- src/main/java/com/fpt/StreamGAP/entity/MusicGame.java
+++ src/main/java/com/fpt/StreamGAP/entity/MusicGame.java
@@ -13,33 +13,21 @@ public class MusicGame {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @JoinColumn(name = "game_id", nullable = true)
     private Integer gameId;
-
-    private Integer score;
-    
-    @Column(length = 20)
-    private String game_type;
-
-    @Column(nullable = true)
+    @Column(length = 50)
     private String question_text;
-
     @Column(nullable = true)
     private String answer_1;
-
     @Column(nullable = true)
     private String answer_2;
     @Column(nullable = true)
     private String answer_3;
     @Column(nullable = true)
     private String answer_4;
-
     @Column(nullable = true)
     private Integer correct_answer;
-    @Column(nullable = true)
-    private String createdByUsername;
 
-    private Integer user_answer;
 
-    private Date played_at;
+
 
     public MusicGame() {
 
@@ -60,25 +48,6 @@ public class MusicGame {
     public void setGameId(Integer gameId) {
         this.gameId = gameId;
     }
-
-
-
-    public Integer getScore() {
-        return score;
-    }
-
-    public void setScore(Integer score) {
-        this.score = score;
-    }
-
-    public String getGame_type() {
-        return game_type;
-    }
-
-    public void setGame_type(String game_type) {
-        this.game_type = game_type;
-    }
-
     public String getQuestion_text() {
         return question_text;
     }
@@ -119,32 +88,4 @@ public class MusicGame {
         this.answer_4 = answer_4;
     }
 
-    public String getCreatedByUsername() {
-        return createdByUsername;
-    }
-
-    public void setCreatedByUsername(String createdByUsername) {
-        this.createdByUsername = createdByUsername;
-    }
-
-    public Integer getUser_answer() {
-        return user_answer;
-    }
-
-    public void setUser_answer(Integer user_answer) {
-        this.user_answer = user_answer;
-    }
-
-    public Date getPlayed_at() {
-        return played_at;
-    }
-
-    public void setPlayed_at(Date played_at) {
-        this.played_at = played_at;
-    }
-
-    @PrePersist
-    protected void onCreate() {
-        this.played_at = new Date(System.currentTimeMillis());
-    }
 }
