8381e0718f134b33dc2d2b485636333a73701ebe
thuanAlex30 <thuandh30@gmail.com>
2024-12-02 15:56:38 +0700

    Update UserManagementController.java

--- src/main/java/com/fpt/StreamGAP/controller/UserManagementController.java
+++ src/main/java/com/fpt/StreamGAP/controller/UserManagementController.java
@@ -10,6 +10,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
