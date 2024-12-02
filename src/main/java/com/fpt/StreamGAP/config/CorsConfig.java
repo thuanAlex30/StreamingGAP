23780695ec3186af2f1fc1ede0f8e38c11698f7d
thuanAlex30 <thuandh30@gmail.com>
2024-12-02 15:15:21 +0700

    Update CorsConfig.java

--- src/main/java/com/fpt/StreamGAP/config/CorsConfig.java
+++ src/main/java/com/fpt/StreamGAP/config/CorsConfig.java
@@ -9,16 +9,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
     @Bean
-    public WebMvcConfigurer webMvcConfigurer(){
+    public WebMvcConfigurer webMvcConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedOrigins("*");
+                        .allowedOrigins("https://streaminggap-fontend.onrender.com"); // Add the specific origin here
             }
         };
     }
+
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
